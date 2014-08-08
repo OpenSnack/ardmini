7f4c04e9d685aaf53a0bc18c4394ada4285258d1
snackthyme <stevenboddez@gmail.com>
2014-08-08 09:45:41 -0600

    Create ardmini.sh

---
+++ ardmini.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+set -e
+
+SAVEIFS=$IFS
+IFS='\'
+SKIP=1
+IGNORE=
+SELECT=
+
+usage(){
+  echo -e "usage: ./ardmini.sh -f path [-s index] [-aceiq]\n"
+  echo options:
+  echo "-a   same as -cei"
+  echo "-c   enables copying *.app in source to /Applications"
+  echo "-e   enables executing *.sh in source"
+  echo "-f   specifies the source folder (REQUIRED)"
+  echo "-h   displays this message"
+  echo "-i   enables installing *.pkg from source"
+  echo "-n   skips the indices specified (numbers separated by commas, 1 is top of folder)"
+  echo "-q   suppresses 'skipping' messages"
+  echo "-s   skip to index in source folder (1 is top of folder)"
+  echo "-y   only processes the indices specified (see -n)"
+  exit 0
+}
+
+while getopts ":aceihqf:n:s:y:" opt; do
+  case $opt in
+    a)
+    COPY=true
+    INSTALL=true
+    SHELL=true
+      ;;
+    c)
+      COPY=true
+      ;;
+    e)
+      SHELL=true
+      ;;
+    i)
+      INSTALL=true
+      ;;
+    h)
+      usage
+      ;;
+    q)
+      NOSKIPMESSAGE=true
+      ;;
+    f)
+      SOURCE=$(echo $OPTARG | sed 's/\/$//;s/\/\//\//g')
+      FILE=true
+      ;;
+    n)
+      FILTER=$(echo $OPTARG | sed 's/[0-9]*/&d;/g;s/,//g;s/$/p/')
+      IGNORING=true
+      ;;
+    s)
+      if [[ $OPTARG -lt 1 ]]
+      then
+        echo -s requires a positive integer, see -h
+        exit 1
+      else
+        SKIP=$OPTARG
+      fi
+      SKIPPING=true
+      ;;
+    y)
+      FILTER=$(echo $OPTARG | sed 's/[0-9]*/&p;/g;s/,//g')
+      SELECTING=true
+      ;;
+    :)
+      echo "a flag is missing an argument (-f, -n, -s, or -y), see -h"
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$(id -u)" != "0" ]; then
+   echo "script must be run as root"
+   exit 1
+fi
+
+if [[ $FILE != true ]]
+then
+  echo -f must be specified, see -h
+  exit 1
+fi
+
+if [[ $IGNORING && $SELECTING ]] || [[ $IGNORING && $SKIPPING ]] || [[ $SELECTING && $SKIPPING ]]
+then
+  echo -e "no combination of -s, -n or -y can be used at the same time"
+  exit 1
+fi
+
+if [[ $IGNORING != true && $SELECTING != true ]]
+then
+  FILTER=p
+fi
+
+echo Started `date`
+find "$SOURCE" -maxdepth 1 -not -path '*/\.*' | sed '1d' | sed -n "$FILTER" | while read f; do
+  if [[ $SKIP != 1 ]]
+  then
+    let "SKIP -= 1"
+  elif [[ $f == *.app && $COPY == true ]]
+  then
+    if [[ -e /Applications/$(basename $f) ]]
+    then
+      echo $(tput setaf 4)replacing$(tput sgr0) /Applications/$(basename $f)
+      rm -rf /Applications/$(basename $f)
+      cp -R "$f" /Applications/$(basename $f)
+    else
+      echo $(tput setaf 4)copying$(tput sgr0) "$f" to /Applications
+      cp -R "$f" /Applications/$(basename $f)
+    fi
+  elif [[ $f == *.pkg && $INSTALL == true ]]
+  then
+    echo $(tput setaf 4)installing$(tput sgr0) "$f"
+    installer -pkg "$f" -target /
+  elif [[ $f == *.sh && $f != *ardmini.sh && $SHELL == true ]]
+  then
+    echo $(tput setaf 4)executing$(tput sgr0) "$f"
+    chmod +x "$f"
+    sh "$f"
+  elif [[ $NOSKIPMESSAGE != true ]]
+  then
+    echo $(tput setaf 5)skipping$(tput sgr0) "$f"
+  else
+    continue
+  fi
+done
+
+echo $(tput setaf 2)done$(tput sgr0)
+
+IFS=$SAVEIFS
